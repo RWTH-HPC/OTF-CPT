ba3347f5fa77851fd319f7db7d4dca71d4850d5d
felix.tomski <tomski@itc.rwth-aachen.de>
2025-09-29 14:08:00 +0200

    Add plotting tests

---
+++ tests/plot/hybrid.sh
@@ -0,0 +1,6 @@
+# RUN: rm -rf %t/out
+# RUN: %plotscript -o %t/out %project_src_dir/script/data
+# RUN: test -f %t/out/data_graph.pdf
+# RUN: test -f %t/out/data_graph.png
+# RUN: test -f %t/out/data_metrics.pdf
+# RUN: test -f %t/out/data_metrics.png
---
+++ tests/plot/mpionly.sh
@@ -0,0 +1,7 @@
+# RUN: rm -rf %t/out
+# RUN: %plotscript --mpi-only -o %t/out %project_src_dir/script/data
+# RUN: test -f %t/out/data_graph.pdf
+# RUN: test -f %t/out/data_graph.png
+# RUN: test -f %t/out/data_metrics.pdf
+# RUN: test -f %t/out/data_metrics.png
+
