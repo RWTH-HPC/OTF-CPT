07798f2ffef82f0d7b633fb4c3e26e4e52e97652
Joachim Jenke <jenke@itc.rwth-aachen.de>
2026-05-15 12:37:32 +0200

    Fix reformated UNSUPPORTED line

--- tests/omp-only/omp-detached-late.c
+++ tests/omp-only/omp-detached-late.c
@@ -3,10 +3,10 @@
 // RUN: %compile_c
 // RUN: env OMP_NUM_THREADS=2 %load_otfcpt %otfcpt_options_dump_stopped \
 // RUN: %t | %FileCheck %s
-// clang-format on
+// UNSUPPORTED: GNU,icc-2021.6,icc-2021.7,icc-2021.9
+// UNSUPPORTED: intelllvm-2022.1,intelllvm-2022.2,intelllvm-2023.1
 
-// UNSUPPORTED:
-// GNU,icc-2021.6,icc-2021.7,icc-2021.9,intelllvm-2022.1,intelllvm-2022.2,intelllvm-2023.1
+// clang-format on
 
 // Note: adapted from task_late_fulfill.c test from LLVM
 
